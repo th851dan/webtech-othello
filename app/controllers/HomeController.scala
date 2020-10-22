b64d8a7df20ae13dcd781fb6397a064844115852
marcothuemmler <marco.thuemmler@me.com>
2020-10-22 10:48:06 +0200

    Add types and Await response in newGame method

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,11 +1,14 @@
 package controllers
 
-import com.google.inject.Guice
+import com.google.inject.{Guice, Injector}
 import de.htwg.se.othello.{BoardModuleServer, OthelloModule, UserModuleServer}
 import de.htwg.se.othello.controller.controllerComponent.ControllerInterface
 import javax.inject._
 import net.codingwell.scalaguice.InjectorExtensions.ScalaInjector
 import play.api.mvc._
+
+import scala.concurrent.Await
+import scala.concurrent.duration.Duration
 
 
 /**
@@ -22,33 +25,33 @@ class HomeController @Inject()(val controllerComponents: ControllerComponents) e
    * will be called when the application receives a `GET` request with
    * a path of `/`.
    */
-    val startBoardServer = BoardModuleServer.main(null)
-    val startUserServer = UserModuleServer.main(null)
-    val injector = Guice.createInjector(new OthelloModule)
-    val gameController = injector.instance[ControllerInterface]
-    val othelloAsText = gameController.boardToString
-  def index() = Action { implicit request: Request[AnyContent] =>
+    val startBoardServer: Unit = BoardModuleServer.main(null)
+    val startUserServer: Unit = UserModuleServer.main(null)
+    val injector: Injector = Guice.createInjector(new OthelloModule)
+    val gameController: ControllerInterface = injector.instance[ControllerInterface]
+    val othelloAsText: String = gameController.boardToString
+  def index: Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
     Ok(views.html.index())
   }
 
-  def othello = Action {
+  def othello: Action[AnyContent] = Action {
     Ok(gameController.boardToString)
   }
 
-  def currentPlayer = Action {
+  def currentPlayer: Action[AnyContent] = Action {
     Ok(gameController.currentPlayer.toString)
   }
 
-  def newGame = Action {
-    gameController.newGame
+  def newGame: Action[AnyContent] = Action {
+    Await.result(gameController.newGame, Duration.Inf)
     Ok(gameController.boardToString)
   }
 
-  def resizeBoard(op: String) = Action {
+  def resizeBoard(op: String): Action[AnyContent] = Action {
     gameController.resizeBoard(op)
     Ok(gameController.boardToString)
   }
-  def set(pos: String) = Action {
+  def set(pos: String): Action[AnyContent] = Action {
     processInputLine(pos)
     Ok(gameController.boardToString)
   }

64ef76f587de9e757754202c57ba002ab0c0901f
marcothuemmler <marco.thuemmler@me.com>
2020-11-17 15:40:51 +0100

    Add othello backend as sbt dependency instead of *.jar

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -10,26 +10,14 @@ import play.api.mvc._
 import scala.concurrent.Await
 import scala.concurrent.duration.Duration
 
-
-/**
- * This controller creates an `Action` to handle HTTP requests to the
- * application's home page.
- */
 @Singleton
 class HomeController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
 
-  /**
-   * Create an Action to render an HTML page.
-   *
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
-    val startBoardServer: Unit = BoardModuleServer.main(null)
-    val startUserServer: Unit = UserModuleServer.main(null)
-    val injector: Injector = Guice.createInjector(new OthelloModule)
-    val gameController: ControllerInterface = injector.instance[ControllerInterface]
-    val othelloAsText: String = gameController.boardToString
+  val startBoardServer: Unit = BoardModuleServer.main(null)
+  val startUserServer: Unit = UserModuleServer.main(null)
+  val injector: Injector = Guice.createInjector(new OthelloModule)
+  val gameController: ControllerInterface = injector.instance[ControllerInterface]
+
   def index: Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
     Ok(views.html.index())
   }

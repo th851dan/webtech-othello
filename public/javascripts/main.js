a57c021855b7173546a6d56e4c61a4d4c9157e05
marcothuemmler <marco.thuemmler@me.com>
2020-11-17 18:49:38 +0100

    Small cosmetic changes

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -4,11 +4,11 @@ document.onreadystatechange = function() {
     const $window = $(window);
 
     function checkWidth() {
-        if ($window.width() < 800) {
+        if ($window.width() < 768) {
             $('#sidebar').removeClass('show');
         }
 
-        if ($window.width() >= 800) {
+        if ($window.width() >= 768) {
             $('#sidebar').addClass('show');
         }
     }

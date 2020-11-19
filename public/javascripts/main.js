af22595363bca9bd62778ccc444d8395b89be30a
marcothuemmler <marco.thuemmler@me.com>
2020-11-19 22:55:09 +0100

    Reduce amount of elements that are repainted on each move and add new animations. Replace XMLHttpRequests with fetch

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -16,60 +16,68 @@ document.onreadystatechange = function() {
     }
 
     checkWidth();
-    updateUI()
+    updateUI();
+    updateDifficulty();
 
     $(window).resize(checkWidth);
 };
 
 function request(endpoint) {
-    fetch(apiUrl + endpoint).then(updateUI)
+    fetch(apiUrl + endpoint).then(updateUI);
 }
 
-function getDifficulty() {
-    const xhr = new XMLHttpRequest();
-    xhr.open("GET", apiUrl + "getdifficulty");
-    xhr.onloadend = () => {
-        ["Easy", "Normal", "Hard"].forEach(d => $('#' + d).removeClass("active text-body"));
-        $('#' + xhr.response).addClass("active text-body");
-        $("#difficulty-div").text(xhr.response);
-    }
-    xhr.send();
+function changeDifficulty(difficulty) {
+    fetch(apiUrl + "difficulty/" + difficulty).then(updateDifficulty);
+}
+
+function updateDifficulty() {
+    fetch(apiUrl + "getdifficulty")
+        .then(response => response.text())
+        .then(difficulty => {
+            ["Easy", "Normal", "Hard"].forEach(d => $('#' + d).removeClass("active text-body"));
+            $('#' + difficulty).addClass("active text-body");
+            $("#difficulty-div").text(difficulty);
+    });
 }
 
 function updateUI() {
     window.history.pushState( null, null,"/othello");
-    getDifficulty();
     reloadBoard();
 }
 
 function set(x, y) {
-    const col = String.fromCharCode(x + 65)
-    const row = 1 + y
+    const col = String.fromCharCode(x + 65);
+    const row = 1 + y;
     fetch(apiUrl + `set/${col}${row}`).then(updateUI);
     setTimeout(reloadBoard, 100);
 }
 
 // reload board without reloading whole page
 function reloadBoard() {
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET', apiUrl + "boardjson");
-    xhr.onloadend = () => repaintBoard(xhr.response);
-    xhr.send();
+    fetch(apiUrl + "boardjson")
+        .then(response => response.json())
+        .then(board => repaintBoard(board));
 }
 
-function repaintBoard(response) {
-    const boardJson = JSON.parse(response)
+function repaintBoard(boardJson) {
     boardJson.squares.forEach(elem => {
         const square = document.getElementById(elem.row + "" + elem.col);
-        square.childNodes.forEach(e => e.remove())
-        if (elem.value > 0) {
-            const child = document.createElement("img")
-            child.setAttribute("src", "assets/images/" + elem.value + ".png")
-            square.appendChild(child)
-        } else if (elem.value < 0) {
-            const child = document.createElement("span");
-            child.setAttribute("class", "dot d-inline-block rounded-circle mt-1");
-            square.appendChild(child)
+        square.childNodes.forEach(e => {
+            if (elem.value === 0 || (elem.value < 0 && e.src) || (elem.value > 0 && (!e.src || !e.src.includes(elem.value)))) {
+                e.remove();
+            }
+        })
+        if (square.childNodes.length === 0) {
+            if (elem.value > 0) {
+                const child = document.createElement("img");
+                child.setAttribute("src", "assets/images/" + elem.value + ".png");
+                child.setAttribute("class", "flip-tile");
+                square.appendChild(child);
+            } else if (elem.value < 0) {
+                const child = document.createElement("span");
+                child.setAttribute("class", "dot d-inline-block rounded-circle mt-1 jelly-dot");
+                square.appendChild(child);
+            }
         }
     });
     $("#black-tiles").text(boardJson.squares.filter(e => e.value === 1).length);

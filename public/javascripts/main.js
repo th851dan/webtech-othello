52fafe79a11a635a93bed1b1ce4a57701e04fd5c
marcothuemmler <marco.thuemmler@me.com>
2020-11-19 16:58:51 +0100

    Set selected difficulty as active in sidebar. Repaint board properly after each move. Replace favicon

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -28,7 +28,11 @@ function request(endpoint) {
 function getDifficulty() {
     const xhr = new XMLHttpRequest();
     xhr.open("GET", apiUrl + "getdifficulty");
-    xhr.onreadystatechange = () => $("#difficulty-div").text(xhr.response)
+    xhr.onloadend = () => {
+        ["Easy", "Normal", "Hard"].forEach(d => $('#' + d).removeClass("active text-body"));
+        $('#' + xhr.response).addClass("active text-body");
+        $("#difficulty-div").text(xhr.response);
+    }
     xhr.send();
 }
 
@@ -41,30 +45,33 @@ function updateUI() {
 function set(x, y) {
     const col = String.fromCharCode(x + 65)
     const row = 1 + y
-    fetch(apiUrl + `set/${col}${row}`).then(updateUI)
+    fetch(apiUrl + `set/${col}${row}`).then(updateUI);
+    setTimeout(reloadBoard, 100);
 }
 
 // reload board without reloading whole page
 function reloadBoard() {
     const xhr = new XMLHttpRequest();
-    xhr.open('GET', apiUrl + "boardjson")
-    xhr.onreadystatechange = () => {
-        const boardJson = JSON.parse(xhr.response)
-        boardJson.squares.forEach(elem => {
-            const square = document.getElementById(elem.row + "" + elem.col);
-            square.childNodes.forEach(e => e.remove())
-            if (elem.value > 0) {
-                const child = document.createElement("img")
-                child.setAttribute("src", "assets/images/" + elem.value + ".png")
-                square.appendChild(child)
-            } else if (elem.value < 0) {
-                const child = document.createElement("span");
-                child.setAttribute("class", "dot d-inline-block rounded-circle mt-1");
-                square.appendChild(child)
-            }
-        });
-        $("#black-tiles").text(boardJson.squares.filter(e => e.value === 1).length);
-        $("#white-tiles").text(boardJson.squares.filter(e => e.value === 2).length);
-    }
+    xhr.open('GET', apiUrl + "boardjson");
+    xhr.onloadend = () => repaintBoard(xhr.response);
     xhr.send();
+}
+
+function repaintBoard(response) {
+    const boardJson = JSON.parse(response)
+    boardJson.squares.forEach(elem => {
+        const square = document.getElementById(elem.row + "" + elem.col);
+        square.childNodes.forEach(e => e.remove())
+        if (elem.value > 0) {
+            const child = document.createElement("img")
+            child.setAttribute("src", "assets/images/" + elem.value + ".png")
+            square.appendChild(child)
+        } else if (elem.value < 0) {
+            const child = document.createElement("span");
+            child.setAttribute("class", "dot d-inline-block rounded-circle mt-1");
+            square.appendChild(child)
+        }
+    });
+    $("#black-tiles").text(boardJson.squares.filter(e => e.value === 1).length);
+    $("#white-tiles").text(boardJson.squares.filter(e => e.value === 2).length);
 }
